9554e9defd4797ceadd14f6dc1022dfb41aad852
akbar <mohakbar180199@gmail.com>
2023-12-12 23:32:33 +0700

    up try label3

--- generate_label.py
+++ generate_label.py
@@ -1,59 +1,52 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics.pairwise import cosine_similarity
-import networkx as nx
 import nltk
-from nltk.tokenize import sent_tokenize
+import re
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+import joblib
+import logging
 
-# Inisialisasi model RandomForest (contoh: di sini diasumsikan model_rf sudah diinisialisasi atau dimuat sebelumnya)
-model_rf = RandomForestClassifier()
+nltk.download("punkt")
+nltk.download("stopwords")
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def preprocess_text(news_text):
+    # Konversi ke huruf kecil
+    lower = news_text.lower()
+
+    # Menghilangkan tanda baca
+    puncuation = [re.sub(r'[.,()&=%:-]', '', token) for token in lower.split()]
+
+    # Menghilangkan angka
+    puncuation = [re.sub(r'\d+', '', token) for token in puncuation]
+
+    # Menghilangkan kata-kata stop
+    stop_words = set(stopwords.words("indonesian"))
+    stopword = [token for token in puncuation if token.lower()
+                not in stop_words]
+    stopword = " ".join(stopword)
+
+    return stopword
 
 
 def get_label(news_text):
-    # Tokenisasi kalimat
+    # Pra-pemrosesan teks
+    preprocessed_text = preprocess_text(news_text)
 
-    nltk.download('punkt')
+    # Melakukan transformasi TF-IDF pada teks yang sudah diproses
+    vectorizer = joblib.load('model/tfidfvectorizer')
+    x_new = vectorizer.transform([preprocessed_text]).toarray()
 
-    new_data = sent_tokenize(news_text)
+    # Memuat model RandomForest yang sudah ada
+    model = joblib.load('model/random_forest_model')
 
-    # Inisialisasi TfidfVectorizer
-    tfidf_vectorizer = TfidfVectorizer()
-    inp_tfidf_matrix = tfidf_vectorizer.fit_transform(new_data)
+    # Melakukan prediksi
+    prediction = model.predict(x_new)
+    result = prediction[0]
 
-    # ========== cosine ==========
-    inp_cos_sim_result = []  # untuk menyimpan hasil cosine sim akhir
+    logging.debug(f"Text after preprocessing: {preprocessed_text}")
+    logging.debug(f"Transformed data shape: {x_new.shape}")
+    logging.debug(f"Prediction array: {prediction}")
 
-    inp_graf = nx.DiGraph()
-    inp_cos_sim = cosine_similarity(inp_tfidf_matrix)
-
-    # inisialisasi indeks awal perulangan dari setiap hasil cosine
-    for i_hasil in range(len(inp_cos_sim)):
-        # inisialisasi indeks kedua perulangan dari setiap hasil cosine
-        for j_hasil in range(i_hasil + 1, len(inp_cos_sim)):
-            inp_cos_sim_result.append(
-                [i_hasil, j_hasil, inp_cos_sim[i_hasil][j_hasil]])
-            inp_graf.add_edge(i_hasil, j_hasil,
-                              weight=inp_cos_sim[i_hasil][j_hasil])
-
-    inp_summary = []  # membuat array kosong untuk hasil inp_summary
-
-    inp_cc = nx.closeness_centrality(inp_graf)
-    inp_cc = dict(sorted(inp_cc.items(),
-                         key=lambda item: item[1], reverse=True))
-
-    for key, value in inp_cc.items():
-        inp_summary.append(new_data[key])
-
-    # Menggunakan model_rf yang telah diinisialisasi sebelumnya
-    summ_inp_tfidf_matrix = tfidf_vectorizer.transform(new_data)
-
-    # Melakukan prediksi menggunakan model_rf
-    result = model_rf.predict(summ_inp_tfidf_matrix.toarray())
-
-    # Mengembalikan hasil prediksi
     return result
-
-# Contoh penggunaan:
-# news_text = "Contoh teks berita yang ingin dikategorikan."
-# result = get_label(news_text)
-# print(result)
